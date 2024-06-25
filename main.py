be7d3968051a3cf9542171d23f7ebf64ac9efb6b
Keshav vaishnav <66151299+Keshav6O9@users.noreply.github.com>
2024-06-25 06:53:17 +0000

    added fastapi app and created phrase methoud using nltk textblob

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-from mylib.logic import wiki
+from mylib.logic import wiki, search_wiki,phrase
+from fastapi import FastAPI
+import uvicorn
 
-print(wiki())
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call/search or /wikiLogic"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    data = search_wiki(value)
+    return {"result": data}
+
+
+@app.get("/wiki/{value}")
+async def wikiLogic(name: str):
+    """Retrieve wikipedia page"""
+
+    result = wiki(name)
+    return {"result": result}
+
+
+@app.get("/phrases/{value}")
+async def phrases(name: str):
+    """Retrieve wikipedia page adn return phrases"""
+
+    result = phrase(name)
+    print("result == ",result)
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080)
